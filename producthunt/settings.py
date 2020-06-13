fe9319ca6fbdbb173e40bd87c72b372a0db07e8a
Nitin Veeraperumal <nveera21@gmail.com>
2020-06-13 11:41:13 -0700

    finishing touches

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -120,3 +120,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "producthunt/static/")]
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
